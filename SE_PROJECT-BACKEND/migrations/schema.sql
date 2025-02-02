fdf382a30f20ecdcfe56a514158eb7f1e4ea3fd7
Karthik <90787191+Karthik-karnam@users.noreply.github.com>
2025-02-02 16:54:24 -0500

    removing directory

--- SE_PROJECT-BACKEND/migrations/schema.sql
+++
@@ -1,9 +0,0 @@
--- Create Users Table
-CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
-);
-
-
-
